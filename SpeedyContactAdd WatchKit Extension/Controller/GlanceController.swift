99ac89fc7bc9c129599845b60d9005498dedd06e
Aaron Haughton <dreamcast2000@outlook.com>
2020-07-19 11:33:03 -0400

    commit

---
+++ SpeedyContactAdd WatchKit Extension/Controller/GlanceController.swift
@@ -0,0 +1,30 @@
+//
+//  GlanceController.swift
+//  SpeedyContactAdd WatchKit Extension
+//
+//  Created by Aaron Haughton on 7/16/16.
+//  Copyright © 2016 AlphaGradeINC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
---
+++ SpeedyContactAdd WatchKit Extension/Controller/NotificationController.swift
@@ -0,0 +1,37 @@
+//
+//  NotificationController.swift
+//  SpeedyContactAdd WatchKit Extension
+//
+//  Created by Aaron Haughton on 7/16/16.
+//  Copyright © 2016 AlphaGradeINC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+    
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func didReceive(_ notification: UNNotification, withCompletion completionHandler: @escaping (WKUserNotificationInterfaceType) -> Swift.Void) {
+        completionHandler(.custom)
+    }
+}
+
